1541a0f0f92b91e6148133b9aebd03f9df46d93a
giordanni <giordanni.formiga@aluno.uepb.edu.br>
2024-06-24 12:21:57 -0300

    Projeto do bootcamp da DIO

---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+public abstract class Conteudo {
+	
+	protected static final double XP_PADRAO = 10; // ao defenir o static estou dizendo que vou poder trabalhar com essa variável fora da minha classe
+	private String titulo;
+	private String descricao;
+	
+	public abstract double calcularXp(); 
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,40 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+	
+	private LocalDate data;
+	
+	
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	@Override
+	public double calcularXp() {
+		// TODO Auto-generated method stub
+		return XP_PADRAO + 20d;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Mentoria{"
+				+ "Titulo: " + super.getTitulo() + ", " 
+				+ "Descrição: " + super.getDescricao() + ", " 
+				+ "data: " + this.data + "}";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
